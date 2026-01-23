979ba5c7fbcc4ecccde10da447c25d83794a2a19
Souvik292002 <mr.souvik10@gmail.com>
2026-01-23 11:13:27 +0530

    cw

--- ClassWork/day29/tcpServ.cpp
+++ ClassWork/day29/tcpServ.cpp
@@ -31,47 +31,31 @@ int main()
     bind(sd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     listen(sd, 5);
 
-    cout << "[server] listening on 127.0.0.1:" << PORT << endl;
+    cout << "[server] waiting...\n";
 
     clientLen = sizeof(client_addr);
     csd = accept(sd, (struct sockaddr*)&client_addr, &clientLen);
-
-    cout << "[server] client connected from "
-         << inet_ntoa(client_addr.sin_addr)
-         << ":" << ntohs(client_addr.sin_port) << endl;
+    cout << "[server] client connected\n";
 
     while (1) {
         memset(msg, 0, MAXBUFF);
         int n = read(csd, msg, MAXBUFF);
+        if (n <= 0) break;
 
-        if (n <= 0) {
-            cout << "[server] client disconnected unexpectedly\n";
-            break;
-        }
-
-        cout << "[server] received from client: " << msg << endl;
-
-        // Client wants to quit
+        // If client types quit
         if (strcmp(msg, "quit") == 0) {
             strcpy(reply, "[server], bye");
             write(csd, reply, strlen(reply));
-
-            cout << "[server] sent to client: [server], bye\n";
-            cout << "[server] closing connection\n";
             break;
         }
 
-        // Normal reply
+        // Normal message
         strcpy(reply, "[server], ");
         strcat(reply, msg);
-
         write(csd, reply, strlen(reply));
-        cout << "[server] sent to client: " << reply << endl;
     }
 
     close(csd);
     close(sd);
-
-    cout << "[server] server shutdown\n";
     return 0;
 }
